6a6e8878874d0e621fe6677852e1f254412dac4a
alex aku <vixndotcom@gmail.com>
2025-12-23 23:25:49 +0100

    go

--- src/home/Video.jsx
+++ src/home/Video.jsx
@@ -1,20 +1,32 @@
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import { health } from '../assets';
 
 const Video = () => {
-
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
 
   return (
-   <div>
-   <video 
-      src={health} 
-      autoPlay 
-      loop 
-      muted 
-      playsInline
-      className='w-full md:h-[600px] md:w-[100vw]  mx-auto object-contain md:object-cover' 
-   />
-</div>
+    <div
+      ref={ref}
+      className="w-full overflow-hidden mx-auto relative md:h-[550px] h-[495px]" // half of desired video height
+    >
+      {inView && (
+        <video
+          src={health}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-auto md:h-[600px] object-contain md:object-cover"
+          
+          style={{ marginBottom: '-50%' }} // move video up to crop bottom half
+          
+        />
+      )}
+    </div>
   );
 };
 
