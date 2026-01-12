481bb53e46adb6b5e526f3b767425866bb37c8c8
alex aku <vixndotcom@gmail.com>
2026-01-13 00:08:21 +0100

    go

--- src/faq/HeadAwards.jsx
+++ src/faq/HeadAwards.jsx
@@ -48,7 +48,7 @@ const HeadAwards = () => {
   }, [])
 
   return (
-    <section className="w-full mt-5 ">
+    <section className="w-full ">
       <div className="max-w-5xl mx-auto ">
         {/* DATE Section - Left Aligned */}
         <div className="flex flex-col items-start mb-10">
