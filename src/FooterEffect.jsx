49dc9efb5a4bcb27180593805b163ef469364801
alex aku <vixndotcom@gmail.com>
2026-01-14 15:01:43 +0100

    go

--- src/FooterEffect.jsx
+++ src/FooterEffect.jsx
@@ -3,13 +3,13 @@ import { ahmed, big, lab, right, sponsor } from './assets'
 
 const FooterEffect = () => {
   return (
-    <div className='w-[100vw] max-w-5xl'>
+    <div className='w-[95vw] max-w-5xl'>
    <div className="image-collage xl:mx-auto">
-  <img src={big} className="img img-main" />
+  <img src={big} className="img img-main " />
   <img src={right} className="img img-left" />
   <div className="block block-left" />
   <div className="block block-right" />
-  <div className="block block-bottom" />
+  <div className="block block-bottom " />
 </div>
 
 </div>
