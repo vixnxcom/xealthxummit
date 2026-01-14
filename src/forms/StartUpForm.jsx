766b3fd87d046878de74f805cdfb738397165ad5
alex aku <vixndotcom@gmail.com>
2026-01-14 08:07:49 +0100

    npm

--- src/forms/StartUpForm.jsx
+++ src/forms/StartUpForm.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const StartUpForm = () => {
   return (
-    <div>
-      
+    <section>
+    <div className="form-wrapper">
+      <div className="form-mask">
+        <iframe
+          title="HealthTech Frontiers 2026 - Start-Up Pass Ticket"
+          src="https://forms.zohopublic.com/vixndotcomgm1/form/HealthTechFrontiers2026/formperma/fLHObU2jpaB0TqKcTwbvNnccRNphKczfDPUjpnX7HqE"
+          frameBorder="0"
+          scrolling="no"
+          className="form-iframe"
+        />
+      </div>
     </div>
+    <div className='h-[80px]'></div>
+    </section>
   )
 }
 
