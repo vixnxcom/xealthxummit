32814fd4f84d058af99d12a9cfb6ffe889785555
alex aku <vixndotcom@gmail.com>
2026-01-15 05:58:31 +0100

    go

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import TypeWriterText from './faq/TypeWriterText'
 
 const Footer = () => {
   return (
     <div className="flex flex-col p-3 space-y-6 ">
       <div className="max-w-5xl w-full mx-auto ">
-        <h1 className="text-[13vw] md:text-[56px] md:px-12  editors-bold font-bold text-blue mb-10 letter-shade 
+        <TypeWriterText>
+   <h1 className="text-[13vw] md:text-[56px] md:px-12  editors-bold font-bold text-blue mb-10 letter-shade 
         ">
           HEALTHTECH <br />
           FRONTIERS <br />
           <span className="editors-itallic">2026</span>
         </h1>
+        </TypeWriterText>
+     
 
         {/* <div className="text-[3vw] text-center md:text-[10px] mb-2 editors-reg text-gray-400 letter-sahde">
          
