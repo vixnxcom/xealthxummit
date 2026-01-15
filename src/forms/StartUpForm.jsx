4fbbf4fc0066d442413e373ece299469caa4adc9
alex aku <vixndotcom@gmail.com>
2026-01-15 05:41:06 +0100

    go

--- src/forms/StartUpForm.jsx
+++ src/forms/StartUpForm.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const StartUpForm = () => {
+  // Scroll to top when component mounts
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <section>
-  <div className="flex flex-col items-center  bg-pass">
-          <h1
-          
-            className="editors-bold  p-3 mt-10 text-[20px] tracking-widest mx-auto  text-white text-center">
-    HealthTech Frontiers - 2026
-      </h1>
-          <h1
-          
-            className="editors-bold  p-3 text-[20px] tracking-widest mx-auto mb-5  text-white text-center">
-       Startup Pass
-      </h1>
-  
-    
-    </div> 
-    <div className='bg-white h-[40px]'></div>
-    <div className="form-wrapper ">
-
-      <div className="form-mask">
-        <iframe
-          title="HealthTech Frontiers 2026 - Start-Up Pass Ticket"
-          src="https://forms.zohopublic.com/vixndotcomgm1/form/HealthTechFrontiers2026/formperma/fLHObU2jpaB0TqKcTwbvNnccRNphKczfDPUjpnX7HqE"
-          frameBorder="0"
-          scrolling="no"
-          className="form-iframe"
-        />
+      <div className="flex flex-col items-center bg-pass">
+        <h1 className="editors-bold p-3 mt-10 text-[20px] tracking-widest mx-auto text-white text-center">
+          HealthTech Frontiers - 2026
+        </h1>
+        <h1 className="editors-bold p-3 text-[20px] tracking-widest mx-auto mb-5 text-white text-center">
+          Startup Pass
+        </h1>
       </div>
-    </div>
- 
+      
+      <div className='bg-white h-[40px]'></div>
+      
+      <div className="form-wrapper">
+        <div className="form-mask">
+          <iframe
+            title="HealthTech Frontiers 2026 - Start-Up Pass Ticket"
+            src="https://forms.zohopublic.com/vixndotcomgm1/form/HealthTechFrontiers2026/formperma/fLHObU2jpaB0TqKcTwbvNnccRNphKczfDPUjpnX7HqE"
+            frameBorder="0"
+            scrolling="no"
+            className="form-iframe"
+          />
+        </div>
+      </div>
 
-
-    <div className='h-[120px]'></div>
+      <div className='h-[120px]'></div>
     </section>
   )
 }
