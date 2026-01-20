3ec48f11a76805448547b48b29d9b18386ccebf9
alex aku <vixndotcom@gmail.com>
2026-01-20 13:50:25 +0100

    go

--- src/SpeechEffect.jsx
+++ src/SpeechEffect.jsx
@@ -3,7 +3,7 @@ import { ahmed, big, lab, right, sponsor } from './assets'
 
 const SpeechEffect = () => {
   return (
-    <div className='w-full h-[500px] overflow-hidden bg-black'>
+    <div className='w-full h-[400px] overflow-hidden bg-black'>
    <div className=" xl:mx-auto">
   <img src={big} className="w-full" />
 
