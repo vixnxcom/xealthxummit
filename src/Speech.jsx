be0af18671b71db85021894de8d60b98671d5cab
alex aku <vixndotcom@gmail.com>
2026-01-25 08:07:05 +0100

    go

--- src/Speech.jsx
+++ src/Speech.jsx
@@ -16,36 +16,45 @@ const Speech = () => {
 <div className="w-full flex justify-center">
   <div className="text-center flex flex-col items-center max-w-screen-xl">
     <div className="border border-t-gray-800 mb-5 w-[92vw] md:w-[60vw]"></div>
-
-   <div className="text-[12px] text-center md:text-center md:text-[10px] mb-2 editors-reg text-gray-600
-   md:text-gray-400 letter-shade">
+<div
+  className="text-[12px] text-center md:text-center md:text-[10px] mb-2 editors-reg text-gray-600
+  md:text-gray-400 letter-shade"
+>
   {(() => {
-    const text = atob("MjAyNiBIZWFsdGhUZWNoIEZyb250aWVycy4gQWxsIFJpZ2h0cyBSZXNlcnZlZA==")
+    const text = atob(
+      "MjAyNiBIZWFsdGhUZWNoIEZyb250aWVycy4gQWxsIFJpZ2h0cyBSZXNlcnZlZA=="
+    )
     return <p>{text}</p>
   })()}
 </div>
 
-    <div className="text-[12px] text-center md:text-center md:text-[10px] mb-2 editors-reg text-gray-600
-    md:text-gray-400 letter-shade">
-      {(() => {
-        const h = atob("aHR0cHM6Ly93YS5tZS8yMzQ5MDM0NDQyNDU4")
-        const t1 = atob("RGVzaWduZWQgYnk=")
-        const t2 = atob("dml4bmRvdGNvbQ==")
+<div
+  className="text-[12px] text-center md:text-center md:text-[10px] mb-2 editors-reg text-gray-600
+  md:text-gray-400 letter-shade"
+>
+  {(() => {
+    const h = atob("aHR0cHM6Ly93YS5tZS8yMzQ5MDM0NDQyNDU4")
+    const t1 = atob("RGVzaWduZWQgYnk=")
+    const t2 = atob("dml4bmRvdGNvbQ==")
 
-        return (
-          <a
-            href={h}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-cyan transition"
-            data-k="x9"
-          >
-            {t1}{" "}
-            <span>{t2}</span>
-          </a>
-        )
-      })()}
-    </div>
+    return (
+      <a
+        href={h}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-cyan transition"
+        data-k="x9"
+      >
+        {t1}{" "}
+        <span className="text-gray-400 md:text-gray-300">
+          {t2}
+        </span>
+      </a>
+    )
+  })()}
+</div>
+
+ 
   </div>
 </div>
       </div>
