283e15e9bbdffae282bac28448f32ee5ee059dfb
alex aku <vixndotcom@gmail.com>
2026-04-06 06:23:32 +0100

    go

--- src/SpeechEffect.jsx
+++ src/SpeechEffect.jsx
@@ -13,14 +13,14 @@ const SpeechEffect = () => {
 // </div>
    <div className="min-h-[400px]  items-center px-3 sm:px-6 md:px-8 lg:px-20  ">
         <TypeWriterText>
-          <h1 className="text-[12vw]  mt-12 md:text-[56px] md:px-12 editors-bold font-bold text-white
+          <h1 className="text-[12vw]  mt-12 md:text-[56px]  editors-bold font-bold text-white
           mb-10 letter-shade">
             HEALTHTECH <br />
             FRONTIERS <br />
             <span className="text-cyan editors-itallic">2026</span>
           </h1>
         </TypeWriterText>
-         <div className=" md:mt-0  md:px-12  md:max-w-5xl">
+         <div className=" md:mt-0   md:max-w-5xl">
                 <TypeWriterText >
                   <p className='
                     editors-reg 
